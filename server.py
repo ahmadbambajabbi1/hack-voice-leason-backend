1ba0b674fcbbfa93375df3c6ccdba2c4d26ecda1
Ahmad Bamba Jabbi <ahmadbambajabbi1@gmail.com>
2025-07-20 22:44:58 +0530

    add necessary thing

--- server.py
+++ server.py
@@ -5,6 +5,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(videos_bp, url_prefix='/api/videos')
-
+# c
 if __name__ == '__main__':
     app.run(debug=True) 
